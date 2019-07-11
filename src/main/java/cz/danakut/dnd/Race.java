b0359d8f55d71ed61ecb85144ee037c2f1c80f8e
Dana Kutálková <kutalkovad@gmail.com>
2019-07-11 09:42:51 +0200

    add Race enum

---
+++ src/main/java/cz/danakut/dnd/Race.java
@@ -0,0 +1,49 @@
+package cz.danakut.dnd;
+
+public enum Race {
+
+    ;
+
+    public enum Dwarf {
+        HILL,
+        MOUNTAIN
+    }
+
+    public enum Elf {
+        HIGH,
+        WOOD,
+        DARK
+    }
+
+    public enum Halfling {
+        LIGHTFOOT,
+        STOUT
+    }
+
+    public enum Human {
+        DEFAULT
+    }
+
+    public enum Dragonborn {
+        DEFAULT
+    }
+
+    public enum Gnome {
+        FOREST,
+        ROCK
+    }
+
+    public enum Half_elf {
+        DEFAULT
+    }
+
+    public enum Half_orc {
+        DEFAULT
+    }
+
+    public enum Tiefling {
+        DEFAULT
+    }
+
+
+}
